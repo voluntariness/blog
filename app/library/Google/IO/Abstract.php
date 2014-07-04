27bf9dd1ca844362b356a8da52f0029982d99a0f
ivan <voluntarin@gmail.com>
2014-07-04 13:44:47 +0800

    oauth login v6

--- app/library/Google/IO/Abstract.php
+++ app/library/Google/IO/Abstract.php
@@ -19,10 +19,10 @@
  * Abstract IO base class
  */
 
-require_once 'Google/Client.php';
-require_once 'Google/IO/Exception.php';
-require_once 'Google/Http/CacheParser.php';
-require_once 'Google/Http/Request.php';
+// require_once 'Google/Client.php';
+// require_once 'Google/IO/Exception.php';
+// require_once 'Google/Http/CacheParser.php';
+// require_once 'Google/Http/Request.php';
 
 abstract class Google_IO_Abstract
 {
