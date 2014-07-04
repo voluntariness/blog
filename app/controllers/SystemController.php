a0553846195b92f2b52bde2b78774ccef18ab31a
ivan <voluntarin@gmail.com>
2014-07-04 16:12:30 +0800

    oauth login v1

--- app/controllers/SystemController.php
+++ app/controllers/SystemController.php
@@ -18,10 +18,10 @@ class SystemController extends BaseController
                 "Contacts API"
                 "Google+ API"
         */
-        if ( ($user = User::where('email', '=', 'voluntarin@gmail.com')->first()) ) {
-            Session::put('user', $user);
-            return '<script> parent.login_success(); </script>';
-        }
+        // if ( ($user = User::where('email', '=', 'voluntarin@gmail.com')->first()) ) {
+        //     Session::put('user', $user);
+        //     return '<script> parent.login_success(); </script>';
+        // }
 
         $client = new Google_Client();
         $client->setClientId( CLIENT_ID );
